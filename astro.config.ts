d3bcdc16240fea53700c67c972042289a724af86
Fatih Kaya <1994274@gmail.com>
2026-01-31 10:26:37 +0300

    fix: update GitHub social link to correct repository URL

--- astro.config.ts
+++ astro.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
         {
           icon: 'github',
           label: 'GitHub',
-          href: 'https://github.com/withastro/starlight',
+          href: 'https://github.com/fatihky/clhub',
         },
       ],
       sidebar: [
