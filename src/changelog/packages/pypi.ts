368902fb0acc8311ce7a54f61022b303e4b15695
Fatih Kaya <1994274@gmail.com>
2026-02-20 10:38:43 +0300

    feat: do not track django

--- src/changelog/packages/pypi.ts
+++ src/changelog/packages/pypi.ts
@@ -3,7 +3,6 @@ import { PACKAGE_MANAGERS, Package } from '../package';
 const pm = PACKAGE_MANAGERS.pypi;
 
 export const pypiPackages: Package[] = [
-  new Package(pm, 'django', 'https://github.com/django/django'),
   new Package(pm, 'flask', 'https://github.com/pallets/flask'),
   new Package(pm, 'requests', 'https://github.com/psf/requests'),
   new Package(
